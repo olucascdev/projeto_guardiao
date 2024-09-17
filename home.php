e3f4054cae40bcb78652d1233e16dae3556528c0
Lucas Correia <lucasdevproject@gmail.com>
2024-09-16 22:13:40 -0300

    .

--- home.php
+++ home.php
@@ -14,13 +14,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/home.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Deu certo</h1>
     <a href="Controller/logout.php"><button>Logout</button></a>
-
+    
     <?php
+    // esse php vai ficar dentro de uma navbar na lateral
         if($_SESSION['acesso'] < 1){
         echo "Visitante";
         
