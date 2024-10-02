84c18b352ce89b085f0a89c2a44c6ecc984987f1
Lucas Correia <lucasdevproject@gmail.com>
2024-10-02 13:05:13 -0300

    colaboradores

--- Colaborador.php
+++ Colaborador.php
@@ -14,7 +14,38 @@ include_once 'Controller/conexao.php';
 </head>
 <body>
     <div class="container">
-        <select name="nome_colaborador" id="nome_colaborador" placeholder="Selecione o Colaborador"></select>
+        <h3>Vincular Colaborador no Questionário</h3>
+        <table class="table table-light table-bordered table-striped table-hover m-5" border="1" cellspacing = 0 cellpadding = 10>
+                    <thead>
+                        <tr>
+                            <th>Colaborador</th>
+                            <th width="12%">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><select class="form-select" name="nome_colaborador" id="nome_colaborador" placeholder="Selecione o Colaborador"></select></td>
+                           <td><a href=""><button class="btn btn-success w-100"><i class="bi bi-plus-circle"></i> Adicionar</button></a></td>
+                        </tr>
+                                                     
+
+                           
+                    </tbody>
+        </table>
+        <h3>Colaboradores Vinculados ao Questionário</h3>
+        <table class="table table-light table-bordered table-striped table-hover m-5" border="1" cellspacing = 0 cellpadding = 10>
+                    <thead>
+                        <tr>
+                            <th>Colaborador</th>
+                            <th width="12%">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <td></td>
+                    <td><a href=""><button class="btn btn-danger w-100"><i class="bi bi-trash3"></i> Excluir</button></a></td>
+                    </tbody>
+        </table>
+
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
