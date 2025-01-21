05f800e70989225eb8263d48a945c1a2d0a10f7e
Lucas Correia <lucasdevproject@gmail.com>
2025-01-21 12:05:18 -0300

    .

--- Controller/config.php
+++ Controller/config.php
@@ -1,16 +1,14 @@
-<?php 
-    $server = 'localhost';
-    $user = 'root';
-    $password = '';
-    $db = 'projeto_guardiao';
+<?php
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'projeto_guardiao';
 
-    $conn = mysqli_connect($server,$user,$password, $db);
+$conn = mysqli_connect($server, $user, $password, $db);
 
-    $server1 = 'localhost';
-    $user1 = 'root';
-    $password1 = '';
-    $db1 = 'sollutions';
+$server1 = 'localhost';
+$user1 = 'root';
+$password1 = '';
+$db1 = 'sollutions';
 
-    $conn1 = mysqli_connect($server1,$user1,$password1,$db1);
-
-?>
+$conn1 = mysqli_connect($server1, $user1, $password1, $db1);
